1c9f3537975340b83f9488552982ea532550dac2
Akira Ishino <akira.ishino@gmail.com>
2023-12-28 14:44:30 +0900

    Refactor system_tray

---
+++ src-tauri/src/app/tray.rs
@@ -0,0 +1,45 @@
+use tauri::{
+    AppHandle,
+    CustomMenuItem,
+    Manager,
+    SystemTray,
+    SystemTrayEvent,
+    SystemTrayMenu,
+    SystemTrayMenuItem,
+};
+
+pub fn generate_system_tray() -> SystemTray {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let show = CustomMenuItem::new("show".to_string(), "Show");
+    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+    let preferences = CustomMenuItem::new("Preferences".to_string(), "Preferences");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(show)
+        .add_item(hide)
+        .add_item(preferences)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(quit);
+    SystemTray::new().with_menu(tray_menu)
+}
+
+pub fn system_tray_event(app: &AppHandle, event: SystemTrayEvent) {
+    match event {
+        SystemTrayEvent::MenuItemClick { id, .. } => {
+            match id.as_str() {
+                "quit" => {
+                    std::process::exit(0);
+                }
+                "show" => {
+                    let window = app.get_window("main").unwrap();
+                    window.show().unwrap();
+                }
+                "hide" => {
+                    let window = app.get_window("main").unwrap();
+                    window.hide().unwrap();
+                }
+                _ => {}
+            }
+        }
+        _ => {}
+    }
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{
-    CustomMenuItem,
-    SystemTray,
-    SystemTrayEvent,
-    SystemTrayMenu,
-    SystemTrayMenuItem,
-};
+mod app;
+use app::tray;
+
 use tauri::Manager;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -16,43 +12,18 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn generate_system_tray() -> SystemTray {
-    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-    let show = CustomMenuItem::new("show".to_string(), "Show");
-    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
-    let preferences = CustomMenuItem::new("Preferences".to_string(), "Preferences");
-    let tray_menu = SystemTrayMenu::new()
-        .add_item(show)
-        .add_item(hide)
-        .add_item(preferences)
-        .add_native_item(SystemTrayMenuItem::Separator)
-        .add_item(quit);
-    SystemTray::new().with_menu(tray_menu)
-}
+#[tokio::main]
+async fn main() {
+    tauri::async_runtime::set(tokio::runtime::Handle::current());
 
-fn main() {
+    // Enable gpu hardware acceleration on Windows
+    //refer to this issue: https://github.com/tauri-apps/tauri/issues/4891
+    std::env::set_var("WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS", "--ignore-gpu-blocklist");
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
-        .system_tray(generate_system_tray())
-        .on_system_tray_event(|app, event| match event {
-            SystemTrayEvent::MenuItemClick { id, .. } => {
-                match id.as_str() {
-                    "quit" => {
-                        std::process::exit(0);
-                    }
-                    "show" => {
-                        let window = app.get_window("main").unwrap();
-                        window.show().unwrap();
-                    }
-                    "hide" => {
-                        let window = app.get_window("main").unwrap();
-                        window.hide().unwrap();
-                    }
-                    _ => {}
-                }
-            }
-            _ => {}
-        })
+        .system_tray(tray::generate_system_tray())
+        .on_system_tray_event(tray::system_tray_event)
         .run(tauri::generate_context!())
         .expect("error while running tauri application")
         // .run(|_app_handle, event| match event {
