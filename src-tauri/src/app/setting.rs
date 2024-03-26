112971ca5fef4e0272bd5ec6b53f8c98180817ec
Akira Ishino <akira.ishino@gmail.com>
2024-03-26 11:18:06 +0900

    Data dir setting
    #4

--- src-tauri/src/app/setting.rs
+++ src-tauri/src/app/setting.rs
@@ -1,36 +1,68 @@
-use serde_json::Value;
-use tauri::{AppHandle, Wry};
-use tauri_plugin_store::{Error, JsonValue, Store, StoreBuilder};
+use std::path::PathBuf;
+use tauri::{AppHandle, Manager, Wry};
+use tauri_plugin_store::{Error, JsonValue, Store, StoreCollection, with_store};
 
-pub struct Setting {
-    store: Store<Wry>
+pub fn init(app: AppHandle) -> Result<(), Error> {
+    with_setting(app.clone(), |store| {
+        store.load().ok();
+        store.save()
+    })
 }
 
-impl Setting {
-    pub fn new(app: &AppHandle) -> Self {
-        let setting_path = app.path_resolver()
-            .app_data_dir()
-            .unwrap()
-            .join("setting.json");
-        let store = StoreBuilder::new(app.clone(), setting_path).build();
-        Self {
-            store,
-        }
-    }
+fn path(app: &AppHandle) -> PathBuf {
+    app.path_resolver()
+        .app_data_dir()
+        .unwrap()
+        .join("settings.dat")
+}
 
-    pub fn insert(&mut self, key: String, value: JsonValue) -> Result<(), Error> {
-        self.store.insert(key, value)
-    }
+pub fn with_setting<T, F: FnOnce(&mut Store<Wry>) -> Result<T, Error>>(
+    app: AppHandle,
+    f: F,
+) -> Result<T, Error> {
+    let stores = app.state::<StoreCollection<Wry>>();
+    let path = path(&app);
+    with_store(app.clone(), stores, path, f)
+}
 
-    pub fn get(&self, key: impl AsRef<str>) -> Option<&Value> {
-        self.store.get(key)
-    }
+#[tauri::command]
+pub fn setting_set(app: AppHandle, key: String, value: JsonValue) -> Result<(), Error> {
+    with_setting(app, |store| {
+        store.insert(key, value)
+    })
+}
 
-    pub fn has(&self, key: impl AsRef<str>) -> bool {
-        self.store.has(key)
-    }
+#[tauri::command]
+pub fn setting_get(app: AppHandle, key: String) -> Result<Option<JsonValue>, Error> {
+    with_setting(app, |store| {
+        Ok(store.get(key).cloned())
+    })
+}
 
-    pub fn delete(&mut self, key: impl AsRef<str>) -> Result<bool, Error> {
-        self.store.delete(key)
-    }
+#[tauri::command]
+pub fn setting_has(app: AppHandle, key: String) -> Result<bool, Error> {
+    with_setting(app, |store| {
+        Ok(store.has(key))
+    })
+}
+
+#[tauri::command]
+pub fn setting_delete(app: AppHandle, key: String) -> Result<bool, Error> {
+    with_setting(app, |store| {
+        store.delete(key)
+    })
+}
+
+#[tauri::command]
+pub fn setting_load(app: AppHandle) -> Result<(), Error> {
+    with_setting(app, |store| {
+        store.load()
+    })
+}
+
+#[tauri::command]
+pub fn setting_save(app: AppHandle) -> Result<(), Error> {
+    with_setting(app, |store| {
+        store.save()
+    })
 }
--- src/Pref.tsx
+++ src/Pref.tsx
@@ -1,8 +1,66 @@
+import { useEffect, useState } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
+
+async function settingSet(key: string, value: any) {
+  return await invoke("setting_set", { key, value });
+}
+
+async function settingGet<T>(key: string) {
+  return await invoke("setting_get", { key }) as T;
+}
+
+async function settingLoad() {
+  return await invoke("setting_load");
+}
+
+async function settingSave() {
+  return await invoke("setting_save");
+}
+
 function Pref() {
+  const [dataDir, setDataDir] = useState("");
+
+  async function storePreferences() {
+    await settingSet("data-dir", dataDir);
+    await settingSave();
+  }
+
+  useEffect(() => {
+    let isMounted = true;
+    (async () => {
+      await settingLoad();
+      const dataDir = await settingGet<string>("data-dir") || "";
+      if (isMounted) {
+        setDataDir(dataDir);
+      }
+    })();
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
   return (
     <div className="container">
       <h1>Preferences</h1>
-      <p>TODO: Add preferences here</p>
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          (async () => {
+            storePreferences();
+          })();
+        }}
+      >
+        <input
+          id="data-dir-input"
+          onChange={(e) => setDataDir(e.currentTarget.value)}
+          placeholder="Data Directory"
+          value={dataDir}
+        />
+        <br />
+        <br />
+        <button type="submit">Save</button>
+      </form>
     </div>
   );
 }
