e214c9a1ef43666807f112af121e2b18516de346
Akira Ishino <akira.ishino@gmail.com>
2024-04-06 09:43:38 +0900

    Search input

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -1,11 +1,18 @@
 import { Link, Outlet } from "react-router-dom";
 import { Settings } from "lucide-react";
+import { Input } from "@/components/ui/input"
 
 function Layout() {
   return (
     <div className="flex min-h-screen w-full flex-col">
       <header className="sticky top-0 h-16 items-center gap-4 bg-transparent px-4">
         <nav className="flex gap-6 text-lg font-medium mt-2">
+          <div className="ml-[550px] w-[1000px]">
+            <Input
+              type="search"
+              className="bg-transparent focus:bg-background pl-8"
+            />
+          </div>
           <div className="ml-auto h-12 pt-2">
             <Link
               to="/settings"
