d5a9e50558b2d202d5d7270b1f63b2d318a6402f
Akira Ishino <akira.ishino@gmail.com>
2024-04-08 13:46:57 +0900

    search results

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 import {
+  SearchLogsResults,
   searchLogs,
 } from "@/lib/api";
 
@@ -10,11 +11,13 @@ export interface SearchPageProps {
 
 export function SearchPage(_props: SearchPageProps) {
   let { state } = useLocation();
+
+  let [hits, setHits] = useState<SearchLogsResults>();
   
   useEffect(() => {
     (async () => {
      const result = await searchLogs(state.query);
-     console.log(result);
+     setHits(result);
     })();
   }, [state.query]);
 
@@ -22,6 +25,22 @@ export function SearchPage(_props: SearchPageProps) {
     <div className="">
       <h1>Search</h1>
       <p>Query: {state.query}</p>
+      <div>
+        {
+          hits?.hits.map((hit) => (
+            <div key={hit.date}>
+              <h2>{hit.date}</h2>
+              <ul>
+                <li>Application Path: {hit.application_path}</li>
+                <li>Application Title: {hit.application_title}</li>
+                <li>Browser Title: {hit.browser_title}</li>
+                <li>Browser URL: {hit.browser_url}</li>
+                <li>File Path: {hit.file_path}</li>
+              </ul>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
